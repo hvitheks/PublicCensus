47e386f83ba64c2fc7a72ab96cb8ac93d541909b
HviteHeksen <hvithex@gmail.com>
2023-08-19 12:18:59 +0400

    Add first screen

--- app/src/main/java/com/example/test2/SignUpActivity.java
+++ app/src/main/java/com/example/test2/SignUpActivity.java
@@ -1,25 +1,19 @@
 package com.example.test2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
-
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Window w = getWindow();
         w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void toPass(View v) {
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        setContentView(R.layout.activity_sign_up);
     }
 }
