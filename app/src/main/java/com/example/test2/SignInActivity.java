40d7c56e61dd98ffe49817d866065837e9e98936
HviteHeksen <hvithex@gmail.com>
2023-08-26 21:08:31 +0400

    Add FireBase SignIn

--- app/src/main/java/com/example/test2/SignInActivity.java
+++ app/src/main/java/com/example/test2/SignInActivity.java
@@ -1,30 +1,98 @@
 package com.example.test2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
+
+    TextView createNewAccount;
+    EditText inputEmail, inputPassword;
+    Button btnLogin;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Window w = getWindow();
-        w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         setContentView(R.layout.activity_sign_in);
+        createNewAccount=findViewById(R.id.createNewAccount);
+
+        inputEmail=findViewById(R.id.inputEmail);
+        inputPassword=findViewById(R.id.inputPassword);
+        btnLogin=findViewById(R.id.btnLogin);
+        progressDialog=new ProgressDialog(this);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+
+
+        createNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignInActivity.this,SignUpActivity.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perforLogin();
+            }
+        });
     }
 
-    public void toEnter(View v) {
-        Intent intent = new Intent(this, FirstStepActivity.class);
-        startActivity(intent);
-    }
+    private void perforLogin() {
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
 
-    public void toSecondActivity(View v) {
-        Intent intent = new Intent(this, SignUpActivity.class);
+
+        if (!email.matches(emailPattern)) {
+            inputEmail.setError("Введите корректную почту");
+        } else if (password.isEmpty() || password.length() < 6) {
+            inputPassword.setError("Введите корректный пароль");
+        } else {
+            progressDialog.setMessage("Вход...");
+            progressDialog.setTitle("Вход");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(SignInActivity.this, "Вход выполнен", Toast.LENGTH_SHORT).show();
+                    }else {
+                        progressDialog.dismiss();
+                        Toast.makeText(SignInActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+        }
+    }
+    private void sendUserToNextActivity() {
+        Intent intent=new Intent(SignInActivity.this,FirstStepActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
 }
